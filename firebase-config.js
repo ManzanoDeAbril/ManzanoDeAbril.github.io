282565ba88fa6d85b121d4168bdc6cc917b80abb
ManzanoDeAbril <ojedamatias229@gmail.com>
2026-09-03 20:41:08 -0400

    agregado base de datos de fireba
    falta ordenar

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+/* Configuración de Firebase
+ * Rellena estos datos con los de TU proyecto "manzano-de-abril".
+ * Los encuentras en: consola Firebase > Configuración del proyecto >
+ * Tus aplicaciones > (crea una app web con el ícono </>) > Configuración (SDK)
+ */
+const firebaseConfig = {
+    apiKey: "AIzaSyAHqo1hix6ho-ZlcW0OdCGZvjk8rEAT-0A",
+    authDomain: "manzano-de-abril.firebaseapp.com",
+    projectId: "manzano-de-abril",
+    storageBucket: "manzano-de-abril.appspot.com",
+    messagingSenderId: "42787448223",
+    appId: "1:42787448223:web:4314708e07b3f275bf89aa"
+};
